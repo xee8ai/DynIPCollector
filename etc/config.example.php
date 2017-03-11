4439b6d988fa6fa230fc24bde31547151846bcc8
xee8ai <xee8ai@users.noreply.github.com>
2017-03-11 21:43:55 +0100

    Began with config & extended .gitignore.

---
+++ etc/config.example.php
@@ -0,0 +1,21 @@
+<?php
+
+// this is only an configuration example
+// please write your own etc/config.php
+
+$config = array(
+
+	'your_host1_abbrev' => array(
+
+		'desc' => 'Host 1 description',
+		'secret' => 'your_host1_shared_secret',
+		'url' => 'your_webserver_url',
+	),
+	'your_host1_abbrev' => array(
+
+		'desc' => 'Host 1 description',
+		'secret' => 'your_host1_shared_secret',
+		'url' => 'your_webserver_url',
+	),
+
+);
